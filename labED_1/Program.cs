68f86f3d01486875c18d0071d513a0079be12e8a
danielammmm <131558007+Danielammmm@users.noreply.github.com>
2023-04-23 19:10:30 -0600

    commit inicial

--- labED_1/Program.cs
+++ labED_1/Program.cs
@@ -2,3 +2,17 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
+namespace InmueblesGT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedir al usuario que ingrese todas las líneas de entrada en un solo string
+            Console.WriteLine("Ingrese todas las entradas separadas por un salto de línea:");
+            string inputString = Console.ReadLine();
+
+            
+        }
+    }
+}
