34b96edb05d0825a6f0684123d92941d2ded7eb9
danielammmm <131558007+Danielammmm@users.noreply.github.com>
2023-04-23 19:58:08 -0600

    commit ordenamiento final

--- labED_1/Program.cs
+++ labED_1/Program.cs
@@ -38,6 +38,44 @@ namespace InmueblesGT
         }
 
         // Función que obtiene las recomendaciones de apartamentos de acuerdo con los requerimientos del cliente
-       
+        static List<int> GetApartmentRecommendations(List<Dictionary<string, bool>> map, List<string> requirements)
+        {
+            List<int> recommendations = new List<int>();
+            List<int> distances = new List<int>();
+
+            // Recorre cada apartamento en el mapa
+            for (int i = 0; i < map.Count; i++)
+            {
+                Dictionary<string, bool> apartment = map[i];
+                bool meetsRequirements = true;
+
+                // Verifica si el apartamento cumple con los requerimientos del cliente
+                foreach (string requirement in requirements)
+                {
+                    if (!apartment.ContainsKey(requirement) || !apartment[requirement])
+                    {
+                        meetsRequirements = false;
+                        break;
+                    }
+                }
+
+                // Si el apartamento cumple con los requerimientos, se calcula la distancia
+                // y se agrega a la lista de recomendaciones
+                if (meetsRequirements)
+                {
+                    int distance = i; // Distancia es igual al índice del apartamento
+                    distances.Add(distance);
+                }
+            }
+
+            // Ordena la lista de recomendaciones por distancia y las agrega a la lista de recomendaciones
+            distances.Sort();
+            foreach (int distance in distances)
+            {
+                recommendations.Add(distance);
+            }
+
+            return recommendations;
+        }
     }
 }
