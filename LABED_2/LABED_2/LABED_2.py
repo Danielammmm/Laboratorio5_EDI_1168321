66c5f5840f0eaf4a3011136a5e58d2aed64a0ddf
Danielammmm <131558007+Danielammmm@users.noreply.github.com>
2023-04-27 20:39:02 -0600

    comienzo de validaciones

--- LABED_2/LABED_2/LABED_2.py
+++ LABED_2/LABED_2/LABED_2.py
@@ -74,4 +74,51 @@ for r in range(100):
                         price[res] = budgets[i]
                         res += 1
 import json
+with open("C:\\Users\\danie\\onedrive\\ED!\\LABED_2\\lab2_ED\\input_challenge2.jsonl") as f:
+    for r in range(100):
+        json_object = f.readline().strip()
+        input_lab = json.loads(json_object)
+        pricee = [0.0] * 1000
+        determinar_vacio = [False, False, False]
+        color_d = 0
+        res = 0
+        idr = [""] * 1000
 
+        if input_lab["input2"]["typeBuilder"] == "Apartments":
+            for item in input_lab["input1"]:
+                if item["builds"]["Apartments"] is not None:
+                    pfs = [a["isPetFriendly"] for a in item["builds"]["Apartments"]]
+                    budgets = [a["price"] for a in item["builds"]["Apartments"]]
+
+                    for i in range(len(item["builds"]["Apartments"])):
+                        if pfs[i] == input_lab["input2"]["wannaPetFriendly"] and budgets[i] <= input_lab["input2"]["budget"]:
+                            idr[res] = item["builds"]["Apartments"][i]["id"]
+                            pricee[res] = budgets[i]
+                            res += 1
+        elif input_lab["input2"]["typeBuilder"] == "Houses":
+            for item in input_lab["input1"]:
+                if item["builds"]["Houses"] is None:
+                    continue
+
+                determinar_vacio[1] = True
+                id_ = [house["id"] for house in item["builds"]["Houses"]]
+                precio = [house["price"] for house in item["builds"]["Houses"]]
+                color = [0] * len(item["builds"]["Houses"])
+
+                for j in range(len(item["builds"]["Houses"])):
+                    house = item["builds"]["Houses"][j]
+                    id_[j] = house["id"]
+                    precio[j] = house["price"]
+
+                    # Asignar el número dependiendo del color de zona de riesgo.
+                    if house["zoneDangerous"] == "Green":
+                        color[j] = 3
+                    elif house["zoneDangerous"] == "Yellow":
+                        color[j] = 2
+                    elif house["zoneDangerous"] == "Orange":
+                        color[j] = 1
+                    elif house["zoneDangerous"] == "Red":
+                        color[j] = 0
+                        if input.input2.typeBuilder == "Houses":
+for i in range(len(input.input1)):
+if input.input1[i].builds.Houses == None:
