faa36912429a29c323bd230a05a3dada84385cc7
Danielammmm <131558007+Danielammmm@users.noreply.github.com>
2023-04-28 19:56:30 -0600

    laboratorio 3 terminado

---
+++ LABED_3/lab3boceto/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+static void Main(string[] args)
+{
+    Console.Title = "Laboratorio no. 3 Daniela Morales";
+    try
+    {
+        var maxBudget = GetMaxBudget(PathAuctions);
+        Console.WriteLine("Este es el presupuesto más alto: " + maxBudget);
+
+        var dpi = GetDpiWithMaxBudget(PathAuctions, maxBudget);
+        Console.WriteLine("Este es el DPI con presupuesto más alto: " + dpi);
+
+        var signature = GenerateSignature(PathCustomers, dpi);
+        Console.WriteLine("Esta es la firma del usuario: " + signature);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("ERROR: " + ex.Message);
+    }
+}
+
+private static int GetMaxBudget(string path)
+{
+    var max = 0;
+    foreach (var line in File.ReadLines(path))
+    {
+        var budgetIndex = line.IndexOf("\"budget\": ");
+        if (budgetIndex == -1) continue;
+
+        var budgetString = line.Substring(budgetIndex + 10).Split(',', '}')[0];
+        var budget = int.Parse(budgetString);
+        if (budget > max) max = budget;
+    }
+    return max;
+}
+
+private static string GetDpiWithMaxBudget(string path, int maxBudget)
+{
+    foreach (var line in File.ReadLines(path))
+    {
+        var dpiIndex = line.IndexOf("\"dpi\": ");
+        if (dpiIndex == -1) continue;
+
+        var dpiString = line.Substring(dpiIndex + 8).Split(',', '}')[0];
+        var dpi = int.Parse(dpiString);
+
+        var budgetIndex = line.IndexOf("\"budget\": ");
+        if (budgetIndex == -1) continue;
+
+        var budgetString = line.Substring(budgetIndex + 10).Split(',', '}')[0];
+        var budget = int.Parse(budgetString);
+
+        if (budget == maxBudget) return dpiString;
+    }
+    throw new ArgumentException("No se obtuvo ninguna información.");
+}
+private static string GenerateSignature(string path, string dpi)
+{
+    foreach (var line in File.ReadLines(path))
+    {
+        var dpiIndex = line.IndexOf("\"dpi\": ");
+        if (dpiIndex == -1) continue;
+
+        var dpiString = line.Substring(dpiIndex + 8).Split(',', '}')[0];
+        if (dpiString != dpi) continue;
+
+        var random = new Random();
+        var signature = string.Join("", Enumerable.Range(0, 10).Select(_ => random.Next(10)));
+        return signature;
+    }
+    throw new ArgumentException("No se obtuvo ninguna información.");
+}
+    }
+}
+
