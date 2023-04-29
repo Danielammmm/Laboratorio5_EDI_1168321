2b1178d6e620a5ff9d44db0780aaa3467396ce76
Danielammmm <131558007+Danielammmm@users.noreply.github.com>
2023-04-28 19:16:55 -0600

    formato de salida del output

--- lab3boceto/lab3boceto/Program.cs
+++ lab3boceto/lab3boceto/Program.cs
@@ -1,5 +1,23 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+ static void Main(string[] args)
+        {
+            Console.Title = "Laboratorio no. 3 Daniela Morales";
+            try
+            {
+                var maxBudget = GetMaxBudget(PathAuctions);
+                Console.WriteLine("Este es el presupuesto más alto: " + maxBudget);
 
+                var dpi = GetDpiWithMaxBudget(PathAuctions, maxBudget);
+                Console.WriteLine("Este es el DPI con presupuesto más alto: " + dpi);
+
+                var signature = GenerateSignature(PathCustomers, dpi);
+                Console.WriteLine("Esta es la firma del usuario: " + signature);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hubo un error: " + ex.Message);
+            }
+        }
 
