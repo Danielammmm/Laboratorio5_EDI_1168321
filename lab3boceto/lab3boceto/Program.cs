57035cb55179ebe93c3814adfa005b782430bac7
Danielammmm <131558007+Danielammmm@users.noreply.github.com>
2023-04-28 19:32:32 -0600

    clase para presupuesto maximo y dpi

--- lab3boceto/lab3boceto/Program.cs
+++ lab3boceto/lab3boceto/Program.cs
@@ -21,3 +21,38 @@ using System.Linq;
             }
         }
 
+private static int GetMaxBudget(string path)
+        {
+            var max = 0;
+            foreach (var line in File.ReadLines(path))
+            {
+                var budgetIndex = line.IndexOf("\"budget\": ");
+                if (budgetIndex == -1) continue;
+
+                var budgetString = line.Substring(budgetIndex + 10).Split(',', '}')[0];
+                var budget = int.Parse(budgetString);
+                if (budget > max) max = budget;
+            }
+            return max;
+        }
+
+        private static string GetDpiWithMaxBudget(string path, int maxBudget)
+        {
+            foreach (var line in File.ReadLines(path))
+            {
+                var dpiIndex = line.IndexOf("\"dpi\": ");
+                if (dpiIndex == -1) continue;
+
+                var dpiString = line.Substring(dpiIndex + 8).Split(',', '}')[0];
+                var dpi = int.Parse(dpiString);
+
+                var budgetIndex = line.IndexOf("\"budget\": ");
+                if (budgetIndex == -1) continue;
+
+                var budgetString = line.Substring(budgetIndex + 10).Split(',', '}')[0];
+                var budget = int.Parse(budgetString);
+
+                if (budget == maxBudget) return dpiString;
+            }
+            throw new ArgumentException("No se encontró DPI para el mayor presupuesto.");
+        }
